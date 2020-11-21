da1a9d62923773a07fbe42bfe8b38c1da0a500e2
Adam W <a@a.a>
2020-11-21 16:12:45 -0500

    removed applet

--- MyApplet.java
+++
@@ -1,9 +0,0 @@
-import java.applet.Applet;
-     import java.awt.Graphics;
-
-     public class MyApplet extends Applet {
-         @Override
-         public void paint(Graphics g) {
-             g.drawString("Hello applet!", 50, 25);
-         }
-     }
